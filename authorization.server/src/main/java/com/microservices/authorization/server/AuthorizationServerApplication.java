0afc338a135422aab9d355b9ca7703bfe1471001
maximbortnik <maximbortnik@gmail.com>
2020-06-18 17:11:15 +0300

    OAuth2 Server

--- authorization.server/src/main/java/com/microservices/authorization/server/AuthorizationServerApplication.java
+++ authorization.server/src/main/java/com/microservices/authorization/server/AuthorizationServerApplication.java
@@ -3,9 +3,7 @@ package com.microservices.authorization.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableResourceServer
 @EnableEurekaClient
 @SpringBootApplication
 public class AuthorizationServerApplication {
